c0fa6cd11dc8ea3ca0eb73c4c898091cb4ca077c
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2022-11-21 23:34:58 +0530

    Update elb.tf

--- elb/elb.tf
+++ elb/elb.tf
@@ -2,24 +2,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_lb" "insecure" {
+resource "aws_lb" "insecureLBTF" {
   name               = "insecure"
   load_balancer_type = "application"
 }
 
-resource "aws_lb_listener" "insecureListener" {
-  load_balancer_arn = aws_lb.insecure.arn
+resource "aws_lb_listener" "insecureListenerTF" {
+  load_balancer_arn = aws_lb.insecureLBTF.arn
   port              = 80
   protocol          = "HTTP"
 }
 
-resource "aws_lb" "secure" {
+resource "aws_lb" "secureLBTF" {
   name               = "secure"
   load_balancer_type = "application"
 }
 
-resource "aws_lb_listener" "secureListener" {
-  load_balancer_arn = aws_lb.secure.arn
+resource "aws_lb_listener" "secureListenerTF" {
+  load_balancer_arn = aws_lb.secureLBTF.arn
   port              = 443
   protocol          = "HTTPS"
 }
